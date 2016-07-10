38cfc539dce3d4cdca7ed96b840bf539264bc500
Angel Sosa <Angel Sosa@AngelSosa-PC>
2016-07-09 22:29:53 -0400

    pruebas de java

---
+++ src/PrimerasPracticasJava/ConstantesYOperadores.java
@@ -0,0 +1,45 @@
+package PrimerasPracticasJava;
+/*
+ * PARA DECLARAR LOS CONSTANTE SE PONE LA PALABRA FINAL ANTES DE LAS VARIABLES
+ * EJ: final Integer EDAD;
+ */
+
+/*
+ * OPERADORES ARITMETICOS
+ *  + : SUMA
+ *  - : RESTA
+ *  * : MULTIPLICACION
+ *  / : DIVISION
+ */
+/*
+ * OPERADORES LOGICOS Y BOOLEANOS
+ * > : MAYOR QUE
+ * < : MENOR QUE
+ * <>:MAYOR QUE Y MENOR QUE
+ * !=:DISTINTO QUE
+ * ==:INGUAL QUE
+ * &&:Y LOGICO
+ * O : O LOGICO
+ */
+/*
+ * OPERADOR DE INCREMENTO Y DECREMENTO
+ * ++ : INCREMENTO
+ * -- : DECREMENTO
+ * +=NUMERO QUE DESEAS INCREMENTAR EJ: +=3
+ * -=NUMERO QUE DESEAS DECREMENTAR EJ: -=3
+ */
+
+//EN CASO DE DIVISION LOS TIPOS DE DATOS QUE SE TIENE QUE UTILIZAR ES DOUBLE O FLOAT
+public class ConstantesYOperadores {
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		a=5;
+		b=7;
+		c=a+b;
+		
+		//INCREMENTAR
+		c++;
+		System.out.println("El resultado es "+c);
+	}
+}
---
+++ src/PrimerasPracticasJava/Variables.java
@@ -0,0 +1,17 @@
+package PrimerasPracticasJava;
+
+public class Variables {
+
+	
+	public static void main(String[] args) {
+		//el valor de la edad tambien puede declararse en la misma linea de la variable
+		//Ejemplo (" byte edad=35; ") solo se utiliza cuando sabes que valor tiene que mantener
+		byte edad;
+		
+		edad=35;
+		
+		System.out.println("tu edad es "+ edad);
+	}
+
+	
+}
