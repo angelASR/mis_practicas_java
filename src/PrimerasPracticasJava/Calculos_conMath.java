1c5bdefa2b8f5db8d144f96f40920e1e00b379b2
Angel Sosa <Angel Sosa@AngelSosa-PC>
2016-07-10 21:18:01 -0400

    practica con math

---
+++ src/PrimerasPracticasJava/Calculos_conMath.java
@@ -0,0 +1,13 @@
+package PrimerasPracticasJava;
+
+public class Calculos_conMath {
+/*
+ * en el parentesis de sqrt deve irse siempre un tipo de datos duble o sino muetsra error
+ */
+	public static void main(String[] args) {
+	double raiz=Math.sqrt(9);
+	
+	System.out.println("esta es la raiz cuadrada de 9 " + raiz);
+	}
+
+}
