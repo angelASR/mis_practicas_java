a49886af901ee06d1d5a810de6db9206827c890e
Angel Sosa <Angel Sosa@AngelSosa-PC>
2016-07-18 23:14:22 -0400

    practicas java

---
+++ src/PrimerasPracticasJava/Bucles_Todo_Los_Ejemplos.java
@@ -0,0 +1,38 @@
+package PrimerasPracticasJava;
+import javax.swing.*;
+public class Bucles_Todo_Los_Ejemplos {
+/*##################################################################################################
+ *TODOS LOS EJEMPLOS DE BUCLES ESTARAN EN ESTA CLASE LOS EJEMPLOS ANTERIORES USARAN EL "//"		   #
+ *SE PONDRAN COMO COMENTARIOS DE UNA LINEA PARA NO EJECUTARSE 									   #
+ *##################################################################################################
+ *
+ */
+	public static void main(String[] args) {
+		
+/*##############################################################################
+ * En este Ej creamos una aplicacion que nos acceso con 					   #
+ * contraseña con JOptionPane												   #
+ * #############################################################################
+ */
+//		String clave="Asr";													   #
+//																			   #
+//		String pass="";														   #
+//Estamos comparando si "clave "= Al pass 									   #
+// 																			   #	
+//		while (clave.equals(pass)==false){									   #
+//			pass=JOptionPane.showInputDialog("Introduce la contraseña");	   #
+//																			   #
+//			if ((clave.equals(pass)==false)){								   #
+//				System.out.println("Contraseña Incorreta");					   #
+//			}																   #
+//		}																	   #
+//		System.out.println("Contraseña correcta");							   #
+//		}																	   #
+//##############################################################################
+		
+/*##############################################################################
+* EJ_2:
+* ##############################################################################
+*/
+}
+}
