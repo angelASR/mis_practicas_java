41a44c06f356f8dc3639653b2129286119d89e85
Angel Sosa <Angel Sosa@AngelSosa-PC>
2016-07-19 20:46:38 -0400

    juego de adivinar con bucle

--- src/PrimerasPracticasJava/Bucles_Todo_Los_Ejemplos.java
+++ src/PrimerasPracticasJava/Bucles_Todo_Los_Ejemplos.java
@@ -1,5 +1,6 @@
 package PrimerasPracticasJava;
 import javax.swing.*;
+import java.util.*;
 public class Bucles_Todo_Los_Ejemplos {
 /*##################################################################################################
  *TODOS LOS EJEMPLOS DE BUCLES ESTARAN EN ESTA CLASE LOS EJEMPLOS ANTERIORES USARAN EL "//"		   #
@@ -30,9 +31,36 @@ public class Bucles_Todo_Los_Ejemplos {
 //		}																	   #
 //##############################################################################
 		
-/*##############################################################################
-* EJ_2:
-* ##############################################################################
+/*####################################################################################
+* EJ_2:juego de adivinar numero
+* utilizaremos random que genera numeros aleatorios
+* multiplicamos por "100" para que el numero sea comprendido en 0 y 100
+* como el metodo random devuel doubles usamos la refuncion para que nos devuelva
+* un numero entero Ej:int aleatorio=(int)(Math.random()*100);
+* en el parentesis despues de = se va el tipo de dato que queremos pasar en 
+* este caso de double a int
+* ####################################################################################
 */
-}
+//		int aleatorio=(int)(Math.random()*100);
+//		Scanner entrada=new Scanner(System.in);
+//		
+//		
+//		int numero=0;
+//		int intentos=0;
+//		
+//		while(numero != aleatorio){
+//			intentos++;
+//			System.out.println("Introduce un numero");
+//			numero=entrada.nextInt();
+//			if (aleatorio<numero){
+//				System.out.println("El numero es mas bajo");
+//				
+//			}
+//			else if (aleatorio>numero){
+//				System.out.println("El numero es mas alto");
+//			}
+//		}
+//		System.out.println("Correcto conseguiste en "+intentos+" Intentos");
+//#####################################################################################
+	}
 }
